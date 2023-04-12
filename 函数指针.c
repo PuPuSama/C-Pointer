b4ca9a212c3cf6ccf4a5c3a42cecf7da69699691
PuPuSama <110659424+PuPuSama@users.noreply.github.com>
2023-04-12 16:48:00 +0800

    Update 函数指针.c

--- 函数指针.c
+++ 函数指针.c
@@ -10,3 +10,22 @@ int main(){
     printf("sum=%d\n",(*p)(8,8));
     system("pause");
 }
+*******************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+//函数指针
+int add(int x,int y){
+    return x+y;
+}
+void printHello(char* name){        //字符串变量本质上是一个字符数组的首地址,所以应该使用指向字符数组的指针来接收，并在函数体内使用指针访问字符串
+    printf("Hello! %s",name);
+}
+int main(){
+    // int (*p)(int,int);
+    // p=&add;
+    // printf("sum=%d\n",(*p)(8,8));
+    void (*p)(char*);
+    p=printHello;
+    p("Ki");
+    system("pause");
+}
