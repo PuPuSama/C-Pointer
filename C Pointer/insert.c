bcbc7b8b8e0c782b6b9e2e3b4138db0322ff56a8
PuPuSama <110659424+PuPuSama@users.noreply.github.com>
2023-04-24 11:29:41 +0800

    Update insert.c

--- C Pointer/insert.c
+++ C Pointer/insert.c
@@ -1,34 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
-struct Node
-{
+struct Node{
     int data;
     struct Node* next;
 };
 struct Node* head;
 void insert(int x){
-    Node* temp=(Node*)malloc(sizeof(int));
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
     temp->data=x;
-    temp->next=NULL;
-    if(head!=NULL)temp->next=head;
+    temp->next=head;
     head=temp;
 };
 void Print(){
-
+    struct Node* temp=head;
+    while (temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
 };
 int main(){
     head=NULL;
-    printf("How many numbers dou you wang to insert?");
+    printf("How many numbers dou you want to insert?");
     int x,i,n;
     scanf("%d",&n);
-    for ( i = 0; i < n; i++)
-    {
-        printf("Enter the number:")
-        scanf("%d",x);
+    for ( i = 0; i < n; i++){
+        printf("Enter the number:");
+        scanf("%d",&x);
         insert(x);
         Print();
+        printf("\n");
     }
-    
-   
     system("pause");
 }
